485d5394caaea1e608cd45dd16cbdef3dbcb4762
GiveMeSomething <minhhthe150277@fpt.edu.vn>
2021-11-01 15:06:51 +0700

    feat: add more specific time to message upload time when display

--- frontend/src/pages/server/components/message/MessageComponent.tsx
+++ frontend/src/pages/server/components/message/MessageComponent.tsx
@@ -19,10 +19,28 @@ const MessageComponent: FunctionComponent<MessageComponentProps> = ({
     const serverTime = timestamp as unknown as number
 
     // Get time from timestamp
-    const messageTime = new Date(serverTime).toLocaleString([], {
-        hour: '2-digit',
-        minute: '2-digit',
-    })
+    const getMessageTime = () =>
+        new Date(serverTime).toLocaleString([], {
+            hour: '2-digit',
+            minute: '2-digit',
+        })
+
+    const getMessageDateString = () => {
+        const currentTime = new Date()
+        const messageTime = new Date(serverTime)
+
+        const diffDay = currentTime.getDate() - messageTime.getDate()
+        const diffMonth = currentTime.getMonth() - messageTime.getMonth()
+        const diffYear = currentTime.getFullYear() - messageTime.getFullYear()
+
+        if (diffDay === 0 && diffMonth === 0 && diffYear === 0) {
+            return 'Today at '
+        } else if (diffDay === 1 && diffMonth === 0 && diffYear === 0) {
+            return 'Yesterday at '
+        } else {
+            return `${messageTime.getDate()}/${messageTime.getMonth()}/${messageTime.getFullYear()} at `
+        }
+    }
 
     return (
         <div className="flex items-center justify-start py-2 text-gray-500">
@@ -36,7 +54,7 @@ const MessageComponent: FunctionComponent<MessageComponentProps> = ({
             <div className="ml-4">
                 <div className="flex items-baseline pb-1">
                     <h3 className="font-bold">{createdBy.username}</h3>
-                    <h5 className="text-slack-text-blur px-2">{messageTime}</h5>
+                    <h5 className="text-slack-text-blur px-2">{`${getMessageDateString()}${getMessageTime()}`}</h5>
                 </div>
                 <h5>{content}</h5>
             </div>
