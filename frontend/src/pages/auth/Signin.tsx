07140d4eb35d3143ad98e42db328c7ab69e223a0
GiveMeSomething <minhhthe150277@fpt.edu.vn>
2021-12-30 17:06:58 +0700

    refactor: re-order component className to the right scope in Signup

--- frontend/src/pages/auth/Signin.tsx
+++ frontend/src/pages/auth/Signin.tsx
@@ -8,7 +8,9 @@ const Signin = () => {
             <div className="grid md:w-1/2 sm:w-full h-full mx-auto">
                 <AuthForm label="🥩" submitLabel="Sign in" action="signin" />
                 <div className="flex flex-col items-center justify-center my-8 border-t-2">
-                    <h4 className="font-bold">Don&apos;t have an account?</h4>
+                    <h2 className="font-semibold">
+                        Don&apos;t have an account?
+                    </h2>
                     <div className="flex items-center justify-center w-full mt-8">
                         <div className="px-10 py-2 rounded-full bg-yellow-500 hover:bg-yellow-600 cursor-pointer">
                             <Link
