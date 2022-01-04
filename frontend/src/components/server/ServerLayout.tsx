6406296101db5c20731ff36c12cd613a202adbd5
GiveMeSomething <minhhthe150277@fpt.edu.vn>
2022-01-04 08:49:10 +0700

    refactor: refactor Server layout, move dependent style to outside function, change styles

--- frontend/src/components/server/ServerLayout.tsx
+++ frontend/src/components/server/ServerLayout.tsx
@@ -16,25 +16,27 @@ interface ServerLayoutProps {}
 const ServerLayout: FunctionComponent<ServerLayoutProps> = () => {
     const isMetaPanelOpen = useAppSelector(selectIsMetaPanelOpen)
 
+    const getMessagesStyle = () => {
+        if (isMetaPanelOpen) {
+            return 'sm:col-span-9 lg:col-span-7'
+        } else {
+            return 'sm:col-span-12 lg:col-span-10'
+        }
+    }
+
     return (
         <div className="fullscreen text-white overflow-hidden">
             <div className="flex flex-col fullsize">
                 <ServerNavbar />
-                <div className="grid grid-cols-12 fullsize">
-                    <div className="col-span-2 bg-slack-sidebar-normal sm:hidden lg:block">
+                <div className="grid grid-cols-12 fullsize text-gray-800 bg-white">
+                    <div className="col-span-2 bg-slack-sidebar-normal sm:hidden lg:block text-white">
                         <ServerSidebar />
                     </div>
-                    <div
-                        className={`${
-                            isMetaPanelOpen
-                                ? 'sm:col-span-9 lg:col-span-7'
-                                : 'sm:col-span-12 lg:col-span-10'
-                        } fullsize bg-white text-gray-800`}
-                    >
+                    <div className={`${getMessagesStyle()} fullsize`}>
                         <ServerMessages />
                     </div>
                     <div
-                        className="col-span-3 fullsize bg-white text-gray-800"
+                        className="col-span-3 fullsize"
                         hidden={!isMetaPanelOpen}
                     >
                         <MetaPanel />
