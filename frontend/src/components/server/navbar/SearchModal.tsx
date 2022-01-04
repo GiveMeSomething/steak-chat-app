f81c95685b4dd432d1ff488456d151198b9a6198
GiveMeSomething <minhhthe150277@fpt.edu.vn>
2022-01-04 09:51:17 +0700

    refactor: refactor SearchModal and SearchOptionButton

--- frontend/src/components/server/navbar/SearchModal.tsx
+++ frontend/src/components/server/navbar/SearchModal.tsx
@@ -2,23 +2,23 @@ import React, { FunctionComponent, useEffect, useState } from 'react'
 import { useForm } from 'react-hook-form'
 import { useAppDispatch, useAppSelector } from 'redux/hooks'
 
-import { selectCurrentChannel } from 'components/server/redux/channels/channels.slice'
 import {
     selectMessages,
     clearSearchMessage,
-    setSearchMessages
+    setSearchMessages,
+    Message
 } from 'components/server/redux/messages/messages.slice'
-
-import { Undefinable } from 'types/commonType'
+import { selectCurrentChannel } from 'components/server/redux/channels/channels.slice'
 
 import { Modal, Icon } from 'semantic-ui-react'
-import SearchOptionButton from './SearchOptionButton'
+import SearchOptionButton, {
+    SearchOptionButtonProps
+} from './SearchOptionButton'
 
 interface SearchModalProps {
     isOpen: boolean
     setOpen: React.Dispatch<React.SetStateAction<boolean>>
 }
-
 interface FormValues {
     keyword: string
 }
@@ -28,7 +28,6 @@ const SearchModal: FunctionComponent<SearchModalProps> = ({
     setOpen
 }) => {
     const [isLoading, setIsLoading] = useState<boolean>(false)
-    const [searchError, setSearchError] = useState<Undefinable<string>>('')
 
     const { handleSubmit, register, setFocus, reset } = useForm<FormValues>()
 
@@ -42,15 +41,36 @@ const SearchModal: FunctionComponent<SearchModalProps> = ({
         }
     }, [isOpen])
 
-    useEffect(() => {
-        if (!messages || messages.length === 0) {
-            setSearchError(
-                'There is nothing in this channel. Try adding a new message 😉'
-            )
-        } else {
-            setSearchError(undefined)
+    const searchOptions: SearchOptionButtonProps[] = [
+        {
+            buttonLabel: 'Messages',
+            iconName: 'comments outline'
+        },
+        {
+            buttonLabel: 'Files',
+            iconName: 'file alternate outline'
+        },
+        {
+            buttonLabel: 'Channels',
+            iconName: 'list alternate outline'
+        },
+        {
+            buttonLabel: 'Users',
+            iconName: 'user outline'
         }
-    }, [messages])
+    ]
+
+    // TODO: Regex is very expensive operation, need to find another way
+    const getSearchMessages = (keyword: string): Message[] => {
+        const messageRegex = new RegExp(`${keyword}`, 'i')
+
+        // Currently only search for messages in currentChannel
+        const result = messages.filter((message) =>
+            messageRegex.test(message.content)
+        )
+
+        return result
+    }
 
     const onClose = () => {
         // Clear all current loading
@@ -67,19 +87,14 @@ const SearchModal: FunctionComponent<SearchModalProps> = ({
     }
 
     const onSubmit = (data: FormValues) => {
-        // Display as normal when submit empty value
         if (!data.keyword || data.keyword.trim().length <= 0) {
+            // Display as normal when submit empty value
             dispatch(clearSearchMessage())
         } else if (messages) {
             setIsLoading(true)
 
-            const searchMessages = [...messages]
-            const messageRegex = new RegExp(`${data.keyword.trim()}`, 'i')
-
-            // Currently only search for messages in currentChannel
-            const result = searchMessages.filter((message) =>
-                messageRegex.test(message.content)
-            )
+            const searchKeyword = data.keyword.trim()
+            const result = getSearchMessages(searchKeyword)
 
             dispatch(setSearchMessages(result))
 
@@ -112,44 +127,28 @@ const SearchModal: FunctionComponent<SearchModalProps> = ({
                     <input
                         {...register('keyword')}
                         placeholder="Search for messages and users"
-                        className="w-full h-full px-4"
+                        className="fullsize px-4"
                         autoComplete="off"
                     />
                 </div>
             </Modal.Header>
             <Modal.Content>
                 <Modal.Description>
-                    {
-                        // Display error if have any
-                        searchError ? (
-                            <h3 className="py-2">{searchError}</h3>
-                        ) : (
-                            <>
-                                <h3 className="py-2">{`Searching in #${currentChannel.name}`}</h3>
-                                <p className="font-light text-slack-text-blur py-2">
-                                    I am looking for ...
-                                </p>
-                                <div className="py-2">
-                                    <SearchOptionButton
-                                        buttonLabel="Messages"
-                                        iconName="comments outline"
-                                    />
-                                    <SearchOptionButton
-                                        buttonLabel="Files"
-                                        iconName="file alternate outline"
-                                    />
-                                    <SearchOptionButton
-                                        buttonLabel="Messages"
-                                        iconName="list alternate outline"
-                                    />
-                                    <SearchOptionButton
-                                        buttonLabel="Messages"
-                                        iconName="user outline"
-                                    />
-                                </div>
-                            </>
-                        )
-                    }
+                    <h3 className="py-2">{`Searching in #${currentChannel.name}`}</h3>
+                    <p className="font-light text-slack-text-blur py-2">
+                        I am looking for ...
+                    </p>
+                    <div className="py-2">
+                        {searchOptions.map(
+                            ({ buttonLabel, iconName }, index) => (
+                                <SearchOptionButton
+                                    key={index}
+                                    buttonLabel={buttonLabel}
+                                    iconName={iconName}
+                                />
+                            )
+                        )}
+                    </div>
                 </Modal.Description>
             </Modal.Content>
         </Modal>
